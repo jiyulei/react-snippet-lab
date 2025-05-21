0985397c3ebb718cdf52b8688229c9f4b3cc5ea7
JerryLei <jerrylei.ui@gmail.com>
2025-05-20 20:36:53 -0700

    Create TicTacToe component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { ShoppingList } from "./components/ShoppingList/ShoppingList";
 import { ContactForm } from "./components/ContactForm.jsx/ContactForm";
 import { TrafficLight } from "./components/TrafficLight/TrafficLight";
 import { CrouselContainer } from "./components/ImageCrousel/CrouselContainer";
+import { TicTacToe } from "./components/TicTacToe/TicTacToe";
 import "./index.css";
 
 function App() {
   return (
     <div className="App">
       <div>React Snippet</div>
-      <ShoppingList />
+      {/* <ShoppingList />
       <ContactForm />
       <TrafficLight />
-      <CrouselContainer />
+      <CrouselContainer /> */}
+      <TicTacToe />
     </div>
   );
 }
